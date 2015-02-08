2fdacae12455e10125c2ddbf0252b03a6f17230b
camshaft <bytheway.cameron@gmail.com>
2015-02-07 19:53:04 -0700

    add trim function

--- render.js
+++ render.js
@@ -7,14 +7,21 @@ var nested = require('postcss-nested');
 var DOM = require('./dom');
 var slice = Array.prototype.slice;
 
-module.exports = function(mod) {
-  var compiler = postcss([nested]);
+module.exports = function(mod, from) {
+  var compiler = postcss([validate, nested]);
   return function(props) {
     var out = mod.render(DOM, $get, props, null, genYield(props));
     var str = Array.isArray(out) ? out.join('') : out;
-    return compiler.process(str).css;
+    return compiler.process(str, {from: from}).css;
   };
 };
+
+function validate(css) {
+  (css.nodes || []).forEach(function(node) {
+    if (!~node.selector.indexOf('&')) return;
+    throw new Error('properties cannot be declared in the root. check ' + node.source.input.from);
+  });
+}
 
 function $get(path, parent, fallback) {
   for (var i = 0; i < path.length; i++) {
@@ -25,6 +32,7 @@ function $get(path, parent, fallback) {
 }
 
 function genYield(props) {
+  props = props || {};
   return function $yield(name) {
     if (!name) return props.children;
     var prop = props[name];
--- test/ess.test.js
+++ test/ess.test.js
@@ -1,12 +1,30 @@
 var should = require('should');
 var ess = require('ess');
 var ast2template = require('ast2template');
+var fs = require('fs');
+var path = require('path');
+var read = fs.readFileSync;
+var readdir = fs.readdirSync;
+var evalM = require('eval');
+var render = require('../render');
 
 describe('ess', function() {
-  it('should work', function() {
+  it('should construct an ast2template render function', function() {
     var ast = ess.file(__dirname + '/cases/constructor.ess');
-    console.log(ast2template(ast, {
+    ast2template(ast, {
       keyName: false
-    }));
+    });
+  });
+
+  readdir('test/cases').forEach(function(file){
+    if (file.charAt(0) === '.') return;
+    it(file, function(){
+      var out = ess.file(__dirname + '/cases/' + file);
+      var str = ast2template(out, {
+        keyName: false
+      });
+      var mod = evalM(str, file, {require: require}, true);
+      render(mod)();
+    });
   });
 });
